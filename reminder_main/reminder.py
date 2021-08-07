b770e01c0617af9bf0ca5feb82837a561011e4a3
nekoshark02 <kyle2020namm@zb.ztv.ne.jp>
2021-08-07 15:07:20 +0900

    commmit

--- reminder_main/reminder.py
+++ reminder_main/reminder.py
@@ -1,14 +1,13 @@
-import calendar
 from flask import Flask
+import calendar
 
 app = Flask(__name__)
-
 @app.route('/')
 def calendar_month():
-    calendar_month = print(calendar.month())
-    return calendar_month
+    calendars = calendar.month()
+    return calendars
 
-if __name__ == "__main__":
-    app.run()
+    if __name__ == "__main__":
+        app.run()
 
 
