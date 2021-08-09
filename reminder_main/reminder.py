9ea360204739737e8a12a32dba6c9c32424511d1
nekoshark02 <kyle2020namm@zb.ztv.ne.jp>
2021-08-09 10:47:47 +0900

    入力できるようにはした

--- reminder_main/reminder.py
+++ reminder_main/reminder.py
@@ -1,13 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask, render_template,request
 import datetime
+
 
 time_n = datetime.datetime.now()
 
 remind = Flask(__name__)
 
 @remind.route("/")
-def reminder_():
-    return render_template('reminder_html')
+def reminder_mainpage():
+    return render_template('reminder.html')
+@remind.route("/ivent",methods=['GET','POST'])
+def reminder_ivent():
+    try:
+        if request.method == 'GET':
+            return request.args.get('query','')
+        elif request.method == 'POST':
+            return request.form['query']
+        else:
+            return 400
+    except Exception as e:
+        return str(e)
     
 if __name__ == "__main__":
     remind.run()
